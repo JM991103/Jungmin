3d0983ffa4909cb9702d85833f8738e641207b91
JM991103 <ljn1103@naver.com>
2022-08-18 20:29:03 +0900

    home 3일차

---
+++ home_1/home_1/Character.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace home_1
+{
+    public class Character
+    {
+        private string name;
+        private int hp = 100;
+        private int maxHP = 100;
+        private int strenth = 10;
+        private int dexterity = 5;
+        private int intellegence = 7;
+
+        //nameArry에 기본값 설정 (선언과 할당을 동시에 함)
+        string[] nameArry = { "전사", "마법사", "궁수", "도적", "해적" };
+
+        Random rand;
+
+        public int HP
+        {
+            get
+            {
+                return hp;
+            }
+            private set
+            {
+                hp = value;
+                if (hp > maxHP)
+                {
+                    hp = maxHP;
+                }
+                if (hp <= 0)
+                {
+                    //사망 처리용 함수 호출
+                    Console.WriteLine($"{name} 이 사망");
+                }
+            }
+        }
+
+        public Character()
+        {
+            rand = new Random();
+            int randomNumber = rand.Next();
+            randomNumber %= 5;
+            name = nameArry[randomNumber];
+
+            GenerateStatus();
+            TestPrintStatus();
+        }
+
+        public Character(string newName)
+        {
+            rand = new Random();
+            name = newName;
+
+            GenerateStatus();
+            TestPrintStatus();
+        }
+
+        private void GenerateStatus()
+        {
+            maxHP = rand.Next(100, 201);
+            hp = maxHP;
+
+            strenth = rand.Next(20)+1;
+            dexterity = rand.Next(20)+1;
+            intellegence = rand.Next(20)+1;
+        }
+
+        public void Attack(Character target)
+        {
+            int damage = strenth;
+            Console.WriteLine($"{name}이 {target.name}에게 공격을 합니다. (공격력 {damage})");
+            target.TakeDamage(damage);
+         }
+
+        public void TakeDamage(int damage)
+        {
+            HP -= damage;
+            Console.WriteLine($"{name}이 {damage}만큼의 피해를 입었습니다.");
+        }
+
+        public void TestPrintStatus()
+        {
+            Console.WriteLine("┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓");
+            Console.WriteLine($"┃ 이름\t: {name}");
+            Console.WriteLine($"┃ HP\t:{hp,4} / {maxHP,4}");
+            Console.WriteLine($"┃ 힘\t: {strenth,2}");
+            Console.WriteLine($"┃ 민첩\t: {dexterity,2}");
+            Console.WriteLine($"┃ 지능\t: {intellegence,2}");
+            Console.WriteLine("┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛");
+        }
+    }
+}
---
+++ home_1/home_1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace home_1
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            Character human1 = new Character();
+            Character human2 = new Character("새한얀");
+
+            human1.Attack(human2);
+            human1.TestPrintStatus();
+            human2.TestPrintStatus();
+            human2.Attack(human1);
+            human2.TestPrintStatus();
+            human1.TestPrintStatus();
+
+
+            Console.ReadLine();
+        }
+    }
+}

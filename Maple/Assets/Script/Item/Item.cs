c92a3cf42219e46596abb37d83306f017c472fcf
JM991103 <ljn1103@naver.com>
2022-11-13 21:09:06 +0900

    maple 아이템 데이터

---
+++ Maple/Assets/Script/Item/Item.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 아이템 1개를 표현할 클래스
+/// </summary>
+public class Item : MonoBehaviour
+{
+    // 아이템데이터 클래스를 가져온다.
+    public ItemData data;
+
+    private void Start()
+    {
+        // 아이템 데이터에 있는 프리펩을 소환한다.
+        Instantiate(data.modelPrefab, transform.position, transform.rotation, transform);
+    }
+
+    // 플레이어가 아이템 근처에서 획득버튼을 누르면 플레이어가 아이템을 습득한다.
+}
